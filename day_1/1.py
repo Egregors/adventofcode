74d2b51de6a7aa5a48e6123f4d1e84819b08de4a
Vadim Iskuchekov <vadim.deus@gmail.com>
2017-12-03 12:44:23 +1000

    d1 p2 (py)

--- day_1/1.py
+++ day_1/1.py
@@ -13,13 +13,33 @@ def solve_captcha(code: str) -> int:
     return s
 
 
+def solve_captcha_2(code: str) -> int:
+    c = list(code)
+    len_ = len(c)
+    c = c + c[:int(len_ / 2)]
+
+    s = 0
+    for i in range(len_):
+        s += int(c[i] if c[i] == c[i + int(len_ / 2)] else 0)
+
+    return s
+
+
 if __name__ == '__main__':
-    # tests
+    # tests 1
     assert solve_captcha('1122') == 3
     assert solve_captcha('1111') == 4
     assert solve_captcha('1234') == 0
     assert solve_captcha('91212129') == 9
 
+    # tests 2
+    assert solve_captcha_2('1212') == 6
+    assert solve_captcha_2('1221') == 0
+    assert solve_captcha_2('123425') == 4
+    assert solve_captcha_2('123123') == 12
+    assert solve_captcha_2('12131415') == 4
+
     # problem
     f = open("input.txt").readline()
-    print(solve_captcha(f))
+    print('part 1: {}'.format(solve_captcha(f)))
+    print('part 2: {}'.format(solve_captcha_2(f)))
