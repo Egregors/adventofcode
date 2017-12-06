e3ba3bd66791fd6509d6d111586fa82be72ed5cd
Vadim Iskuchekov <root@egregors.com>
2017-12-07 00:15:44 +1000

    d3 p1 (py)

---
+++ day_3/3.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+
+class Grid:
+    U = '^'
+    R = '>'
+    D = 'v'
+    L = '<'
+
+    def __init__(self, x: int):
+        self.goal = x
+        self.s = list()
+
+        self.s.append({
+            'n': 1,
+            'd': self.R,
+            'x': 0,
+            'y': 0
+        })
+
+        self.exist = set()
+        self.exist.add('0;0')
+
+        self._alloc()
+
+    def _forward(self, i: int) -> dict:
+        current = self.s[i - 1]
+        next_ = {'n': i + 1}
+
+        if current['d'] == self.U:
+            next_['x'] = current['x']
+            next_['y'] = current['y'] + 1
+            next_['d'] = self.U if '{};{}'.format(next_['x'] - 1, next_['y']) in self.exist else self.L
+
+        elif current['d'] == self.R:
+            next_['x'] = current['x'] + 1
+            next_['y'] = current['y']
+            next_['d'] = self.R if '{};{}'.format(next_['x'], next_['y'] + 1) in self.exist else self.U
+
+        elif current['d'] == self.D:
+            next_['x'] = current['x']
+            next_['y'] = current['y'] - 1
+            next_['d'] = self.D if '{};{}'.format(next_['x'] + 1, next_['y']) in self.exist else self.R
+
+        elif current['d'] == self.L:
+            next_['x'] = current['x'] - 1
+            next_['y'] = current['y']
+            next_['d'] = self.L if '{};{}'.format(next_['x'], next_['y'] - 1) in self.exist else self.D
+
+        else:
+            raise ValueError()
+
+        self.exist.add('{};{}'.format(next_['x'], next_['y']))
+        return next_
+
+    def _alloc(self):
+        for i in range(1, self.goal):
+            if i % 10000 == 0:
+                print(i)
+            self.s.append(self._forward(i))
+
+    def solve(self):
+        return abs(0 - self.s[self.goal - 1]['x']) + abs(0 - self.s[self.goal - 1]['y'])
+
+
+if __name__ == '__main__':
+    # tests
+    assert Grid(1).solve() == 0
+    assert Grid(12).solve() == 3
+    assert Grid(23).solve() == 2
+    assert Grid(1024).solve() == 31
+
+    # problems
+    print('steps: {}'.format(Grid(312051).solve()))
