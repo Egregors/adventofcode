69602fae9bfc6fd8884e65bd02c52686c8f36efd
Vadim Iskuchekov <root@egregors.com>
2019-08-26 13:26:13 +0200

    d1 p1

--- 2016/d7/Main.hs
+++ 2016/d7/Main.hs
@@ -5,12 +5,35 @@ import Data.List.Split
 import Data.List
 
 main :: IO()
-main = do
-  let s = "abba[mnop]qrst"
-  putStrLn $ intercalate " " $ getWords s
+main = interact $ show . getTLSCount . words
 
-getWords :: String -> [String]
-getWords s = splitOneOf "[]" s
+type WordPairs = (Int, String)
 
-isTLS :: String -> Bool
-isTLS s = 
+getTLSCount xs = length $ filter isLineTLS xs
+getWords xs p = [ snd x | x <- getPairs $ splitOneOf "[]" xs, p $ fst x ]
+getPairs x = zip [1..] x
+
+-- "abcdefg" -> ["abcd", "bcde", "cdef", "defg"]
+splitBy4 xs
+  | length xs <= 4 = [xs]
+  | otherwise = [h] ++ splitBy4 t
+  where
+    h = take 4 xs
+    t = drop 1 xs
+
+isWordTLS s
+  | length s /= 4 = False
+  | length g == 3 && head g == last g = True
+  | otherwise = False
+  where
+    g = group s
+
+-- ioxxoj[asdfgh]zxcvbn -> True
+isLineTLS :: String -> Bool
+isLineTLS s
+  | any isWordTLS $ concat $ map splitBy4 $ getWords s even = False
+  | any isWordTLS $ concat $ map splitBy4 $ getWords s odd = True
+  | otherwise = False
+
+
+
